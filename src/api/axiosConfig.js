79d62a04af242690bd37cfc2b25d17f673ee4312
Vera-Okafor121 <talk2mimi2@gmail.com>
2026-09-21 00:38:38 +0200

    upated cicd

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://18.221.67.127:8080',
+    baseURL:'http://100.54.102.189:8080',
     headers: {
         'Content-Type': 'application/json',
     },
